5bd4daab97db306b286180aa4dbe669e4dbb79aa
MatteoGaloni <mtgaloni@gmail.com>
2023-04-26 22:13:17 +0200

    add click function

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-console.log(1);
+// L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
+// Ogni cella contiene un numero progressivo, da 1 a 100.
+// Ci saranno quindi 10 caselle per ognuna delle 10 righe.
+let wrapper = document.getElementById("wrapper");
+let btn = document.getElementById("btn");
+btn.addEventListener("click", function () {
+  for (let c = 1; c <= 100; c++) {
+    let box = document.createElement("div");
+    box.classList.add("box");
+    wrapper.appendChild(box);
+    box.innerHTML = c;
+    console.log(box);
+  }
+});
+
+// Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
