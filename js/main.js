e1a9b83663abf0911e629d122c42a516691439a0
MatteoGaloni <mtgaloni@gmail.com>
2023-04-27 10:54:56 +0200

    fix oneclick on event listener

--- js/main.js
+++ js/main.js
@@ -3,14 +3,23 @@
 // Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 let wrapper = document.getElementById("wrapper");
 let btn = document.getElementById("btn");
+let oneClick = false;
+console.log(oneClick);
+
 btn.addEventListener("click", function () {
-  for (let c = 1; c <= 100; c++) {
-    let box = document.createElement("div");
-    box.classList.add("box");
-    wrapper.appendChild(box);
-    box.innerHTML = c;
-    console.log(box);
+  if (oneClick == false) {
+    for (let c = 1; c <= 100; c++) {
+      let box = document.createElement("div");
+      box.classList.add("box");
+      wrapper.appendChild(box);
+      box.textContent = c;
+      box.addEventListener("click", function () {
+        this.classList.toggle("selected");
+        console.log(box);
+      });
+    }
   }
+  oneClick = true;
 });
 
 // Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
